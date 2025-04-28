d92365128f326b5c2971c7ff9210d3118bf411ca
= <shavkatovm99@gmail.com>
2025-04-28 19:47:52 +0500

    add env variable to allowed hosts

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # my-django-app
+
+```
+export ALLOWED_HOSTS="example.com,www.example.com,127.0.0.1"
+```
+
+```
+cp .env.example .env
+source .env
+```
--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w^o)uniskp-u6_#l@=t_o65#*@n2919k7ztt9p2m(x2f^ifr4^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'mirafzal.uz']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
